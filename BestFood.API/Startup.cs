513fbf36349b00584b5ecad4fe981bf6ba206a17
Stoil Stoyanov <stoyanov.stoil@gmail.com>
2019-02-11 13:35:07 +0100

    ConnectionString to configuration

--- BestFood.API/Startup.cs
+++ BestFood.API/Startup.cs
@@ -8,18 +8,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BestFood.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            var connetionString = "Server=.;Database=BestFoodDB;Trusted_Connection=True;";
+            var connetionString = Configuration["connectionStrings:bestFoodDBConnectionString"];
             services.AddDbContext<BestFoodContext>(x => x.UseSqlServer(connetionString));
         }
 
